b849b647d7f9d267e07b57f15edc3ae4d5e9d364
Zac Dunn <zac.dunn@garvin-allen.com>
2022-09-09 12:58:37 -0300

    Moved upload to its own view

---
+++ Arcloud/Controllers/UploadController.cs
@@ -0,0 +1,36 @@
+﻿using Arcloud.Models;
+using Arcloud.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arcloud.Controllers
+{
+    public class UploadController : Controller
+    {
+        private readonly AppDbContext appDbContext;
+        private readonly IUploadRepository uploadRepository;
+
+        public UploadController(AppDbContext appDbContext, IUploadRepository uploadRepository)
+        {
+            this.appDbContext = appDbContext;
+            this.uploadRepository = uploadRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(UploadViewModel uploadVm)
+        {
+            var uploads = uploadRepository.AllUploads;
+            if (ModelState.IsValid)
+            {
+                // Add upload to DB
+                return RedirectToAction("UploadComplete");
+            }
+
+            return View(uploadVm);
+        }
+    }
+}
